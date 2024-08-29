81030e626ec10046f952602190538817d80f75cd
Pritiraj Partho <dpartho5616@gmail.com>
2024-08-29 13:56:53 +0600

    mobile app promotion component

---
+++ src/pages/components/MobileApp/MobileApp.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import mobildImg from '../../../../public/mobile-app-three.png';
+import appleImg from '../../../../public/app-store.webp';
+import googleImg from '../../../../public/play-store.webp';
+
+const MobileApp = () => {
+    return (
+        <section className='grid grid-cols-2 lg:mt-24 lg:mb-40 border-2 border-red-600'>
+            <div className='mt-6'>
+                <h1 className='text-2xl  text-center font-bold font-poppins'>Make your Life easier with our mobile app</h1>
+                <p className='text-xl mt-4 text-center font-agdasima'>Food is our common ground, a universal experience.So, try Panda-dish on the mobile app.</p>
+                <div className=' mt-6 flex justify-center gap-12'>
+                    <img className='rounded' src={appleImg} alt="Apple" />
+                    <img className='rounded' src={googleImg} alt="Google" />
+                </div>
+            </div>
+            <div className='justify-self-center'>
+                <img className='w-[500px]' src={mobildImg} alt="" />
+            </div>
+        </section>
+    );
+};
+
+export default MobileApp;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Attraction from '../components/Attraction/Attraction';
 import GetDishes from '../components/Dishes/GetDishes';
 import Banner from '../components/Banner/Banner';
+import MobileApp from '../components/MobileApp/MobileApp';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <GetDishes></GetDishes>
+            <MobileApp></MobileApp>
         </div>
     );
 };
