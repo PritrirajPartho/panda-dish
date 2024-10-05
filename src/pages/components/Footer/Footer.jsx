520a1a5b29eae3ec9da9c04dbcbba0885864c0c1
Pritiraj Partho <dpartho5616@gmail.com>
2024-10-05 13:35:26 +0600

    responsive footer

--- src/pages/components/Footer/Footer.jsx
+++ src/pages/components/Footer/Footer.jsx
@@ -3,39 +3,39 @@ import logo from "../../../../public/logo.png";
 
 const Footer = () => {
     return (
-    <section className='width-control-container'>
-        <footer className='mt-[100px] relative bg-white grid grid-cols-4 gap-4 place-items-center'>
-            <div>
-                <div className='absolute top-0 flex justify-start items-center'>
-                    <img className='rounded-xl ' src={logo} alt="" />
-                    <h1 className='ml-2 font-poppins text-2xl '>Panda Dish</h1>
+    <section className='width-control-container text-center text-3xl sm:text-2xl md:text-xl sm:text-left '>
+        <footer className='mt-[100px] bg-red-400 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 place-items-center  md:px-8 py-6'>
+            <div className='sm:w-[100px] md:w-full'>
+                <div className='mb-2 sm:mt-8 md:mt-0 md:mb-4 flex flex-row justify-center sm:justify-start  md:justify-cetner md:items-center'>
+                    <img className='rounded-xl w-[15%]' src={logo} alt="" />
+                    <h1 className='ml-2 font-poppins  mt-1 sm:mt-0'>Panda Dish</h1>
                 </div>
-                <div className=' mt-12 text-xl font-agdasima'>
+                <div className='font-agdasima'>
                   <p className='mb-2'>Alhamra-zindabazar, Sylhet, BD</p>
                   <p className='mb-2'><strong>Email:</strong> panda@foods.com</p>
                   <p className='mb-2'><strong>Phone:</strong> +01764676589</p>
                 </div>
             </div>
             <div className=' flex flex-col'>
-                <h6 className="footer-title text-2xl mb-3 font-poppins">Services</h6>
-                <a className="link link-hover text-xl font-agdasima mb-2">Branding</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Design</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Marketing</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Advertisement</a>
+                <h6 className="footer-title  mb-3 font-poppins">Services</h6>
+                <a className="link link-hover  font-agdasima mb-2">Branding</a>
+                <a className="link link-hover  font-agdasima mb-2">Design</a>
+                <a className="link link-hover  font-agdasima mb-2">Marketing</a>
+                <a className="link link-hover  font-agdasima mb-2">Advertisement</a>
             </div>
-            <div className='flex flex-col'>
-                <h6 className="footer-title text-2xl mb-3 font-poppins" >Legal</h6>
-                <a className="link link-hover text-xl font-agdasima mb-2">Terms of use</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Privacy policy</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Cookie policy</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Legal policy</a>
+            <div className='hidden sm:flex sm:flex-col sm:mr-8 md:mr-0'>
+                <h6 className="footer-title  mb-3 font-poppins" >Legal</h6>
+                <a className="link link-hover  font-agdasima mb-2">Terms of use</a>
+                <a className="link link-hover  font-agdasima mb-2">Privacy policy</a>
+                <a className="link link-hover  font-agdasima mb-2">Cookie policy</a>
+                <a className="link link-hover  font-agdasima mb-2">Legal policy</a>
             </div>
-            <div className='flex flex-col '>
-                <h6 className="footer-title text-2xl mb-3 font-poppins">Company</h6>
-                <a className="link link-hover text-xl font-agdasima mb-2">About us</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Contact</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Jobs</a>
-                <a className="link link-hover text-xl font-agdasima mb-2">Press kit</a>
+            <div className='flex flex-col sm:mr-8 md:mr-0'>
+                <h6 className="footer-title    mb-3 font-poppins">Company</h6>
+                <a className="link link-hover  font-agdasima mb-2">About us</a>
+                <a className="link link-hover  font-agdasima mb-2">Contact</a>
+                <a className="link link-hover  font-agdasima mb-2">Jobs</a>
+                <a className="link link-hover  font-agdasima mb-2">Press kit</a>
             </div>
         </footer>
     </section>
